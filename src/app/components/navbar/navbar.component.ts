cc16e6525780e6b409477c1849a3f33e3b7a5f0e
345AYUSH420 <singhalayush40@gmail.com>
2023-09-15 12:20:37 +0530

    second

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faListAlt } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
@@ -10,20 +10,33 @@ export class NavbarComponent implements OnInit {
   angellist = faListAlt;
 
   ngOnInit(): void {
-    document.getElementById('home')?.scrollIntoView({ behavior: 'smooth' });
+    // This is the initial scroll behavior when the component is loaded.
+    // Ensure you have an element with the ID 'home' in your HTML.
+    this.scrollToElement('home');
   }
+
+  // Function to scroll to the specified element smoothly.
+  scrollToElement(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  // Functions to handle menu item clicks.
   toHome() {
-    document.getElementById('home')?.scrollIntoView({ behavior: 'smooth' });
+    this.scrollToElement('home');
   }
+
   toAbout() {
-    document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' });
+    this.scrollToElement('about');
   }
+
   toProjects() {
-    document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' });
+    this.scrollToElement('projects');
   }
+
   toExp() {
-    document
-      .getElementById('experience')
-      ?.scrollIntoView({ behavior: 'smooth' });
+    this.scrollToElement('experience');
   }
 }
